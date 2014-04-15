afd1f0450a04c84b70359c0f6edf2ee60cef4b85
Steven <Steven@192.168.1.68>
2014-04-15 18:03:38 +0200

    Création du model : Sous_Secteur

---
+++ model/Sous_Secteur.php
@@ -0,0 +1,31 @@
+<?php
+use Doctrine\ORM\Mapping\Entity as Entity;
+
+namespace Model;
+/** @Entity */
+class Sous_Secteur
+{
+    /** @Id @Column(type="integer") @GeneratedValue */
+    private $id;
+    
+    /** @Column(type="string") */
+    private $title;
+   
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+ 
+}
+?>
