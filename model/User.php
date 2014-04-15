9c57154d3c79f1eb73f4e48572a49c239b390152
Steven <Steven@192.168.1.68>
2014-04-15 18:09:24 +0200

    Création du model : User

--- model/User.php
+++ model/User.php
@@ -3,52 +3,83 @@ use Doctrine\ORM\Mapping\Entity as Entity;
 
 namespace Model;
 /** @Entity */
-class User
+class Type_Annonce
 {
     /** @Id @Column(type="integer") @GeneratedValue */
     private $id;
     
     /** @Column(type="string") */
-    private $name;
+    private $nom;
     
     /** @Column(type="string") */
-    private $login;
-    
+    private $prenom;
+   
     /** @Column(type="string") */
     private $password;
     
-
+    /** @Column(type="string") */
+    private $mail;
+   
+    /** @Column(type="string") */
+    private $key;
+    
+    /** @Column(type="boolean") */
+    private $activated;
     
     public function getId() {
         return $this->id;
     }
 
-    public function setId($id) {
-        $this->id = $id;
+    public function getNom() {
+        return $this->nom;
     }
 
-    public function getName() {
-        return $this->name;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function getLogin() {
-        return $this->login;
-    }
-
-    public function setLogin($login) {
-        $this->login = $login;
+    public function getPrenom() {
+        return $this->prenom;
     }
 
     public function getPassword() {
         return $this->password;
     }
 
+    public function getMail() {
+        return $this->mail;
+    }
+
+    public function getKey() {
+        return $this->key;
+    }
+
+    public function getActivated() {
+        return $this->activated;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    public function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+
     public function setPassword($password) {
         $this->password = $password;
+    }
+
+    public function setMail($mail) {
+        $this->mail = $mail;
+    }
+
+    public function setKey($key) {
+        $this->key = $key;
+    }
+
+    public function setActivated($activated) {
+        $this->activated = $activated;
     }
 
 
